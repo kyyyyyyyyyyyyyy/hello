1d1d12528c0a9d8f86ccf4bb00c9b90f599621d8
haruhky <muhammadrizkyy135@gmail.com>
2024-05-13 22:05:33 +0700

    add parameter

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/kyyyyyyyyyyyyyy/hello
+module github.com/kyyyyyyyyyyyyyy/hello/v2
 
 go 1.22.2
--- hello.go
+++ hello.go
@@ -1,5 +1,5 @@
 package hello
 
-func SayHello() string {
-	return "hello world"
+func SayHello(name string) string {
+	return "hello " + name
 }
